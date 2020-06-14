fc904a86dd46d02531871b1eaf663f463e394dda
tabathafroes <tabathafroes@gmail.com>
2020-06-14 16:41:14 -0300

    updates

---
+++ Software/JobsScreen-master/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import Welcome from './Componets/Welcome'
+import Header from './Componets/header'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+      <Header />
+    <Router>
+      <div>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/vagas" component={App} />
+      </div>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
